8011ed63f578e808ec189e8ae3be1a5f3b337a99
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-06-02 23:32:44 +0000

    Initial Commit

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = (matrix) => {
+  let transpose = [];
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = 0; c < matrix[r].length; c++) {
+      if (r === 0) {
+        transpose.push([matrix[r][c]]);
+      } else {
+        transpose[c].push(matrix[r][c]);
+      }
     }
+  }
+  return transpose;
 }
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  let isInRows = false;
+  let isInColoumns = false;
+  const horizontalJoin = letters.map(ls => ls.join(''))
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      isInRows = true;
+      break;
+    }
+  }
+
+  const VerticalJoin = transpose(letters).map(ls => ls.join(''))
+  for (l of VerticalJoin) {
+    if (l.includes(word)) {
+      isInColoumns = true
+      break;
+    }
+  }
+  return isInRows || isInColoumns;
+}
+module.exports = wordSearch;
